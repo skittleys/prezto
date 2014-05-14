09d70054cf4ec94a2389d6911f86b24bb640f481
skittleys <skittleys@users.noreply.github.com>
2014-05-14 00:00:25 -0400

    My configs for zsh history behaviour

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -24,12 +24,13 @@ setopt INC_APPEND_HISTORY        # Write to the history file immediately, not wh
 setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+#setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate of an older one (not just the last one)
 setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
 setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+#setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
-setopt HIST_BEEP                 # Beep when accessing non-existent history.
+#setopt HIST_BEEP                 # Beep when accessing non-existent history.
+setopt HIST_REDUCE_BLANKS         # remove superfluous blanks
 
 #
 # Aliases
