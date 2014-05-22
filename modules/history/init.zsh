701d89380e0774f8973c8d1c77ecf66ba97988ca
skittleys <skittleys@users.noreply.github.com>
2014-05-22 07:06:20 -0400

    ensuring .zhistory always goes to $HOME

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -11,7 +11,7 @@
 # Variables
 #
 
-HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
+HISTFILE="$HOME/.zhistory"       # The path to the history file.
 HISTSIZE=10000                   # The maximum number of events to save in the internal history.
 SAVEHIST=10000                   # The maximum number of events to save in the history file.
 
