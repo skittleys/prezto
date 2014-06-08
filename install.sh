9b624489a8423274b61a43c489cb3f454fe7bf9d
skittleys <skittleys@users.noreply.github.com>
2014-06-08 11:54:59 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # Easily install Prezto on new setups
 # Thanks to Sorin Ionescu's README
 # and Chionsas' PKGBUILD in the AUR (https://aur.archlinux.org/packages/prezto-git/)
@@ -10,26 +12,24 @@
 setopt EXTENDED_GLOB
 
 # want to make symlinks to all runcoms except zshrc
-for rcfile in "/usr/lib/prezto/runcoms/^(zshrc|README.md); do
-  ln -vs "$rcfile" "$HOME/.${rcfile:t}"
+for rcfile in /usr/lib/prezto/runcoms/^(zshrc|README.md); do
+  sudo ln -vs "$rcfile" "$HOME/.${rcfile:t}"
 done
 
 # link zsh files in /etc to runcoms (zshrc after)
-zlist=('/etc/zsh/zlogin 'etc/zsh/zlogout' 'etc/zsh/zpreztorc' 'etc/zsh/zshenv' 'etc/zsh/zshrc')
+zlist=('/etc/zsh/zlogin' 'etc/zsh/zlogout' 'etc/zsh/zpreztorc' 'etc/zsh/zshenv')
 for zfile in ${zlist[@]}; do
 	rcfile=$(basename $zfile)
 	if [ -f /usr/lib/prezto/runcoms/$rcfile ]; then
-		echo "source /usr/lib/prezto/runcoms/$rcfile" >| /etc/zsh/$rcfile
-		echo "/etc/$rcfile modified"
+		sudo zsh -c "echo source /usr/lib/prezto/runcoms/$rcfile >| /etc/zsh/$rcfile" && echo "/etc/zsh/$rcfile modified"
 	fi
 done
 
 
 # make /etc/zsh/zshrc
-echo -e "source /etc/zsh/zpreztorc\nsource /usr/lib/prezto/init.zsh\nsource /usr/lib/prezto/runcoms/zshrc" >| /etc/zsh/zshrc
-
+sudo bash -c "echo -e 'source /etc/zsh/zpreztorc\nsource /usr/lib/prezto/init.zsh\nsource /usr/lib/prezto/runcoms/zshrc' >| /etc/zsh/zshrc" && echo "/etc/zsh/zshrc modified"
 
 # create a user-specific .zshrc
 [[ -e $HOME/.zshrc ]] && cp $HOME/.zshrc $HOME/zshrc-backup && echo "backed up .zshrc to zshrc-backup"
-echo -e "source /usr/lib/prezto/runcoms/zshrc\n\nDEFAULT_USER="`whoami` >| $HOME/.zshrc
+echo -e "source /usr/lib/prezto/runcoms/zshrc\n\nDEFAULT_USER="`whoami` >| $HOME/.zshrc && echo "created .zshrc"
 
