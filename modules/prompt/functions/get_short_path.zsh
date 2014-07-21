e998a14e33779d843809393b33ba34b665db51b0
skittleys <skittleys@users.noreply.github.com>
2014-07-21 12:48:15 -0400

    replace /home/usr/ with ~ always

--- modules/prompt/functions/get_short_path.zsh
+++ modules/prompt/functions/get_short_path.zsh
@@ -5,16 +5,19 @@
 # requires PWD_LENGTH variable to be set
 # defaults to 30, if not set
 function get_short_path() {
-        HOME_TILDE=~
-        LONG_PATH=`pwd`
-        if [[ $LONG_PATH == $HOME_TILDE ]]; then
-                LONG_PATH="~"
-        fi
+    HOME_TILDE=~
+    LONG_PATH=$(pwd)
+    if [[ $LONG_PATH == $HOME_TILDE ]]; then
+        LONG_PATH="~"
+    elif [[ $HOME == ${LONG_PATH:0:${#HOME}} ]]; then
+        LONG_PATH="~${LONG_PATH:${#HOME}}"
+    fi
 
-        # check to see if the prompt path length has been specified
-        if [ ! -n "$PWD_LENGTH" ]; then
-                export PWD_LENGTH=30
-        fi
+    # check to see if the prompt path length has been specified
+    if [ ! -n "$PWD_LENGTH" ]; then
+            export PWD_LENGTH=30
+    fi
+
 
     if [ ${#LONG_PATH} -gt $PWD_LENGTH ]; then
             echo "...${LONG_PATH: -$PWD_LENGTH}"
